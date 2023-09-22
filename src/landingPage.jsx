ec343335279d05de09f4db9b6434ad9d2f15fd1b
Sava2901 <savasergiu2006@gmail.com>
2023-09-22 23:45:22 +0300

    aici scrie oprea

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,44 +1,78 @@
+import './landingPage.css'
+
 function Navbar() {
 
     return (
-        <>
+        <div id="navSection">
             <div className="logo">
                 <img src="#" alt="logo" />
             </div>
-            <nav>
-                <ul>
-                    <button className="home">Home</button>
-                    <button className="courses">Courses</button>
-                    <button className="skillExchange">Skill Exchange</button>
-                    <button className="aboutUs">About Us</button>
-                    <button className="contact">Contact</button>
-                </ul>
-            </nav>
-            <div className="loginContainer">
-                <button className="login">Login</button>
+            <nav className="navContainer">
+                <button className="home">Home</button>
+                <button className="courses">Courses</button>
+                <button className="skillExchange">Skill Exchange</button>
+                <button className="aboutUs">About Us</button>
+                <button className="contact">Contact</button>
                 <button className="signUp">Sign up</button>
-            </div>
-        </>
+            </nav>
+        </div>
     )
 }
 
 function HeroContainer() {
 
     return (
-        <>
-            <div className="heroContainer">
-                <div className="textSide">
-                    <h1>Learm more things <br /> Do more things</h1>
-                    <h4>Sign Up Now!</h4>
-                    <button className="heroButton">Sign Up</button>
-                </div>
-                <div className="imageSide">
-                    <img src="#" alt="heroPhoto" />
-                </div>
+        <div id="heroContainer">
+            <div className="textSide">
+                <h1>Learm more things <br /> Do more things</h1>
+                <h4>Sign Up Now!</h4>
+                <button className="heroButton">Sign Up</button>
             </div>
-        </>
+            <div className="imageSide">
+                <img src="#" alt="heroPhoto" />
+            </div>
+        </div>
     )
 }
 
+function CourseHighlights() {
 
-export {Navbar , HeroContainer}
+    return (
+        <div id="highlightsContainer">
+            <h1>Course Highlights</h1>
+            <div className="highlights">
+                <div className="highlightCard"></div>
+            </div>
+            <button>See More Courses</button>
+        </div>
+    )
+}
+
+function SkillSpotlight() {
+
+    return (
+        <div id="spotlightContainer">
+            <div className="spotlightCard"></div>
+        </div>
+    )
+}
+
+function CallToAction() {
+
+    return (
+        <div id="callToAction">
+
+        </div>
+    )
+}
+
+function Footer() {
+
+    return (
+        <div id="footer">
+
+        </div>
+    )
+}
+
+export { Navbar, HeroContainer, CourseHighlights, SkillSpotlight, CallToAction, Footer }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {Navbar , HeroContainer} from './landingPage'
+import { Navbar, HeroContainer , CourseHighlights , SkillSpotlight , CallToAction, Footer} from './landingPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <Navbar />
     <HeroContainer />
+    <CourseHighlights />
+    <SkillSpotlight />
+    <CallToAction />
+    <Footer />
+
   </React.StrictMode>
 )
