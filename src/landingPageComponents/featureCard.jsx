5f9cc2ce87063ec1999d83b06a5e167e7415cb67
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-09-24 21:39:38 +0300

    Fixed feature card closing

--- src/landingPageComponents/featureCard.jsx
+++ src/landingPageComponents/featureCard.jsx
@@ -1,17 +1,13 @@
 import React, { useRef, useEffect } from "react";
-const featuresContainer = document.querySelector(".featuresContainer");
 
 
-function useOutsideAlerter(ref) {
+/*function useOutsideAlerter(ref) {
     useEffect(() => {
         function handleClickOutside(event) {
             if (ref.current && !ref.current.contains(event.target)) {
-                {ref.current.style.flex = "1";}
+                detects click outside ref
             }
-            else
-            {
-                {ref.current.style.flex = "4";}
-            }
+            
         }
         
         document.addEventListener("mousedown", handleClickOutside);
@@ -19,19 +15,17 @@ function useOutsideAlerter(ref) {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [ref]);
-}
+}*/
 
-export default function FeatureCard() {
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef);
+export default function FeatureCard({isActive, onShow}) {
     const styles = {
         feature:{
-            flex: 1
+            flex: isActive ? 4 : 1
         }
     };
 
     return (
-        <div ref={wrapperRef} className="feature" style={styles.feature}>
+        <div className="feature" style={styles.feature} onClick={onShow}>
             <div className="featureContent">Content</div>
         </div>
     );
--- src/landingPageComponents/features.jsx
+++ src/landingPageComponents/features.jsx
@@ -3,13 +3,13 @@ import FeatureCard from "./featureCard"
 import { useState } from "react"
 
 function Features () {
-    const [click, handleClick] = useState(false);
+    const [active, setActive] = useState(0);
 
     return (
         <div id="featuresContainer">
-            <FeatureCard />
-            <FeatureCard />
-            <FeatureCard />
+            <FeatureCard isActive = {active == 0} onShow = {()=>setActive(0)}/>
+            <FeatureCard isActive = {active == 1} onShow = {()=>setActive(1)}/>
+            <FeatureCard isActive = {active == 2} onShow = {()=>setActive(2)}/>
         </div>
     )
 }
