b3504983a506d92d63b8adb1907e57e2150b8d5e
Sava2901 <savasergiu2006@gmail.com>
2023-09-25 16:18:10 +0300

    done the darkmode button

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,24 +1,43 @@
 import './css/navbar.css'
 import './css/text.css'
+import darkModeImage from './assets/moon.png'
+import lightModeImage from './assets/sun.png'
+import { useState } from 'react'
 
 function Navbar() {
-
     return (
         <div id="navSection">
             <div className="logo">
                 <img src="#" alt="logo" />
             </div>
             <nav className="navContainer">
-                <button className="home text">Home</button>
-                <button className="courses text">Courses</button>
-                <button className="skillExchange text">Skill Exchange</button>
-                <button className="aboutUs text">About Us</button>
-                <button className="contact text">Contact</button>
-                <button className="darkMode">
-                    <img src="./assets/sun.png" alt="" />
-                </button>
-                <button className="signUp text">Sign up</button>
+                <button className="navbarButton home text">Home</button>
+                <button className="navbarButton courses text">Courses</button>
+                <button className="navbarButton skillExchange text">Skill Exchange</button>
+                <button className="navbarButton aboutUs text">About Us</button>
+                <button className="navbarButton contact text">Contact</button>
+                <DarkModeButton />
+                <button className="navbarButton signUp text">Sign up</button>
             </nav>
+        </div>
+    )
+}
+
+function DarkModeButton(){
+    const [isDark, setIsDark] = useState(lightModeImage)
+
+    function handleClick(){
+        if(isDark === darkModeImage){
+            setIsDark(lightModeImage);
+        }
+        else{
+            setIsDark(darkModeImage);
+        }
+    }
+
+    return(
+        <div className=" darkmodeButton" onClick={handleClick}>
+            <img src={isDark} alt="color skema" />
         </div>
     )
 }
