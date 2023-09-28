7a752f6ce39f6c0dd3deb1ec3330c99bca998e4a
Sava2901 <savasergiu2006@gmail.com>
2023-09-28 15:35:49 +0300

    removed unused files and unused code

--- src/landingPageComponents/highlights.jsx
+++ src/landingPageComponents/highlights.jsx
@@ -1,4 +1,3 @@
-// import HighlightCard from "./highlightCard"
 import placeholder from "../assets/placeholder.png"
 import placeholder2 from "../assets/paceholder2.png"
 import ImageSlider from "../imageSlider.jsx"
@@ -16,12 +15,6 @@ function CourseHighlights() {
     return (
         <div id="highlightsContainer">
             <div className="highlights">
-                {/* <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard /> */}
                 <ImageSlider slides={slides}/>
             </div>
         </div>
