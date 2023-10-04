5996de017989a231b6ac8924df3fb5bf00a51b2f
Sava2901 <savasergiu2006@gmail.com>
2023-10-04 11:16:29 +0300

    done the footer breakpoint still need to work on the alignment of the text

--- src/footer.jsx
+++ src/footer.jsx
@@ -11,23 +11,23 @@ function Footer() {
             <div className="text footerColumn">
                 <div className="columnTitle">About us</div>
                 <div className="columnItems">
+                    <div className="columnItem">FAQ</div>
                     <div className="columnItem">Home</div>
                     <div className="columnItem">Contact us</div>
-                    <div className="columnItem">FAQ</div>
                 </div>
             </div>
-            <div className="text footerColumn">
+            <div className="text footerColumn productCollumn">
                 <div className="columnTitle">Product</div>
-                <div className="columnItems">
+                <div className="columnItems productCollumn">
                     <div className="columnItem">Course</div>
-                    <div className="columnItem">Skill Exchange</div>
                     <div className="columnItem">Placeholder</div>
+                    <div className="columnItem">Skill Exchange</div>
                 </div>
             </div>
-            <div className="text footerColumn">
+            <div className="text footerColumn newsletter">
                 <div className="columnTitle">Newsletter</div>
                 <form>
-                    <label htmlFor="email">Sign up for our spam free newsletter</label>
+                    <label className="formText" htmlFor="email">Sign up for our spam free newsletter</label>
                     <div className="container">
                         <input type="email" />
                         <button className="text primary">Subscribe</button>
