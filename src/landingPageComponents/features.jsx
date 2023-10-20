8f06ba7e84e310da620b093dd852e2aed16cb65b
Sava2901 <savasergiu2006@gmail.com>
2023-10-20 16:18:48 +0300

    made feature cards get parameters

--- src/landingPageComponents/features.jsx
+++ src/landingPageComponents/features.jsx
@@ -1,5 +1,8 @@
 import FeatureCard from "./featureCard"
 import "../css/landingPageCss/features.css"
+import { FaRegLightbulb } from "react-icons/fa6"
+import { FaEarthAmericas } from "react-icons/fa6"
+import { SlWallet } from "react-icons/sl"
 
 function Features() {
 
@@ -7,9 +10,9 @@ function Features() {
         <div id="featuresContainer">
             <h1>Features</h1>
             <div className="features">
-                <FeatureCard src={"#"} />
-                <FeatureCard src={"#"} />
-                <FeatureCard src={"#"} />
+                <FeatureCard Component={<FaRegLightbulb/>} Feature={""} Description={""}/>
+                <FeatureCard Component={<FaEarthAmericas/>}  Feature={""} Description={""}/>
+                <FeatureCard Component={<SlWallet/>}  Feature={""} Description={""}/>
             </div>
         </div>
     )
