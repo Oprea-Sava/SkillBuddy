eea341b4856d4a3494cd395add06c535c9fdc4cb
Sava2901 <savasergiu2006@gmail.com>
2023-10-23 11:43:12 +0300

    modified featured course

--- src/landingPageComponents/highlights.jsx
+++ src/landingPageComponents/highlights.jsx
@@ -7,46 +7,25 @@ function CourseHighlights() {
 
 
     return (
-        <div id="highlightsContainer">
-            <div className="featuredCourse">
+        <div id="hook">
+            <div className="hookContainer">
                 <img src={placeholder2} alt="image" />
-                <div className="featuredCourseDetails text">
-                    <div className="featuredCourseTitle">
-                        <div className="featureCircle"></div>
-                        <div>Featured Course</div>
+                <div className="details">
+                    <div className="text hookStatement">
+                        We provide the<br />
+                        best
+                        <span className="specialText"> online courses</span>
                     </div>
-                    <div className="featuredCourseDescription">
-                        <div>Example course for beginers</div>
-                        <div>
-                            ajshdjkabdjaba sgd ajsg dajsgdagsgdjas asjgdahgdasdh
-                            ashsgdjahfahjkdh gsksjerhkjshdkjf dlkjfdfjhlkdmd
-                            ajshdjkabdjaba sgd ajsg dajsgdagsgdjas asjgdahgdasdh
-                            ashsgdjahfahjkdh gsksjerhkjshdkjf dlkjfdfjhlkdmd 
-                        </div>
-                    </div>
-                    <div className="featuredCoursePoints">
-                        <div>
-                            <div className="featureCircle"></div>
-                            <div className="point">feature</div>
-                        </div>
-                        <div>
-                            <div className="featureCircle"></div>
-                            <div className="point">feature</div>
-                        </div>
-                        <div>
-                            <div className="featureCircle"></div>
-                            <div className="point">feature</div>
-                        </div>
-                        <div>
-                            <div className="featureCircle"></div>
-                            <div className="point">feature</div>
-                        </div>
+                    <div className="text hookReason">
+                        ajshdjkabdjaba sgd ajsg dajsgdagsgdjas asjgdahgdasdh
+                        ashsgdjahfahjkdh gsksjerhkjshdkjf dlkjfdfjhlkdmd
+                        ajshdjkabdjaba sgd ajsg dajsgdagsgdjas asjgdahgdasdh
+                        ashsgdjahfahjkdh gsksjerhkjshdkjf dlkjfdfjhlkdmd
                     </div>
                 </div>
             </div>
-
             <div>
-                <ImageCarousel/>
+                <ImageCarousel />
             </div>
         </div>
     )
