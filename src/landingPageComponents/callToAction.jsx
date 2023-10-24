847ada5fdc7ae7f35ff1bd0889a9421b12db9ec5
Sava2901 <savasergiu2006@gmail.com>
2023-10-24 18:57:49 +0300

    resolved responsiveness issues

--- src/landingPageComponents/callToAction.jsx
+++ src/landingPageComponents/callToAction.jsx
@@ -8,7 +8,7 @@ function CallToAction() {
                 <div className="text bigText">Join Our Community Today</div>
                 <div className="text smallText">Start your jurney to lifelong learning and self-improvement</div>
             </div>
-            <button className="text secondary ctaButton">Get Started</button>
+            <button className="text accent ctaButton">Get Started</button>
         </div>
     )
 }
