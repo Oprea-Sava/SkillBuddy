2db9dbac9449e3d12280dcdd2eef0fedc1163d73
Sava2901 <savasergiu2006@gmail.com>
2023-11-11 13:16:37 +0200

    remade the order history table (nonresponsive)

--- src/components/dashboard/orderHistory.jsx
+++ src/components/dashboard/orderHistory.jsx
@@ -2,6 +2,43 @@ import React from "react";
 import "../../css/dashboard/orderHistory.css";
 
 function OrderHisory() {
+	const orderInfo = [
+		{
+			orderId: "Order ID",
+			courseName: "Course Name",
+			date: "Date",
+			price: "Price",
+			status: "Status",
+		},
+	]
+
+	const orders = [
+		{
+			orderId: "#2643",
+			courseName:
+				"Build Responsive Real World Websites " +
+				"with HTML5 and CSS3 Learn and Understand AngularJS " +
+				"to become a professional developer",
+			date: "24 March 2023",
+			price: "$34",
+			status: "On Hold",
+		},
+		{
+			orderId: "#2614",
+			courseName: "Information About UI/UX Design Degree",
+			date: "17 March 2023",
+			price: "free",
+			status: "On Hold",
+		},
+		{
+			orderId: "#2643",
+			courseName: "Information About UI/UX Design Degree",
+			date: "29 January 2023",
+			price: "$558",
+			status: "Completed",
+		},
+	];
+
 	return (
 		<>
 			<div id="orderHistory">
@@ -23,7 +60,41 @@ function OrderHisory() {
 							</form>
 						</div>
 					</div>
-					
+
+					<div className="orderTableContainer">
+						<div className="orderTable text">
+							{orderInfo.map((info) => (
+								<>
+								<div className="orderId orderTableColumnTitle">
+										{info.orderId}
+									</div>
+									<div className="courseName orderTableColumnTitle">
+										{info.courseName}
+									</div>
+									<div className="date orderTableColumnTitle">{info.date}</div>
+									<div className="price orderTableColumnTitle">{info.price}</div>
+									<div className="status orderTableColumnTitle">{info.status}</div>
+									<div></div>
+								</>
+							))}
+							{orders.map((order) => (
+								<>
+									<div className="orderId">
+										{order.orderId}
+									</div>
+									<div className="courseName">
+										{order.courseName}
+									</div>
+									<div className="date">{order.date}</div>
+									<div className="price">{order.price}</div>
+									<div className="status">{order.status}</div>
+									<div className="downloadButtonWrapper">
+										<button className="downloadButton"></button>
+									</div>
+								</>
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
