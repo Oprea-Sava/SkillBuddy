0f22df81a5d9c00fb4cf0aa65adb7449a7d5e883
Sava2901 <savasergiu2006@gmail.com>
2023-11-28 11:58:40 +0200

    designed coursecard

--- client/src/components/dashboard/courseCard.jsx
+++ client/src/components/dashboard/courseCard.jsx
@@ -6,8 +6,19 @@ function CourseCard() {
         <>
             <div className="courseCard">
                 <div className="courseCardImageHolder">
+                    <div className="coursePrice">free</div>
                 </div>
-                <div></div>
+                <div className="courseCardDetails">
+                    <div>
+                        <div className="authorName">Author Name</div>
+                        <button className="bookmark">btn</button>
+                    </div>
+                    <div>Course Name</div>
+                    {/* if the name is too long the button gets out of the card */}
+                    <div>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
             </div>
         </>
     );
