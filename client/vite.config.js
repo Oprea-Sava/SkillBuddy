1f0b98f28467e02bc532c98f075f692e98fb274b
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-12-01 18:44:18 +0200

    Changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-toastify/dist/ReactToastify.css"],
+      external: ["react-toastify/dist/ReactToastify.css", "react-toastify"],
     },
   },
 });
