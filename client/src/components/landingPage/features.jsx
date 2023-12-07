40f7b516af4f0127cb4502d2c70f5ad44e5edefd
Sava2901 <savasergiu2006@gmail.com>
2023-12-07 13:54:32 +0200

    added animations (needs more work)

--- client/src/components/landingPage/features.jsx
+++ client/src/components/landingPage/features.jsx
@@ -4,40 +4,113 @@ import { FaRegLightbulb } from "react-icons/fa6";
 import { FaEarthAmericas } from "react-icons/fa6";
 import { SlWallet } from "react-icons/sl";
 import { useTheme } from "../../themeContext";
+import { useEffect, useRef } from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
 
 function Features() {
 	const { theme } = useTheme();
-    let iconTheme = "dark";
+	let iconTheme = "dark";
 
-    if(theme == "light"){
-        iconTheme = "black";
-    }
-    else {
-        iconTheme = "#bbcbff";
-    }
+	if (theme == "light") {
+		iconTheme = "black";
+	} else {
+		iconTheme = "#bbcbff";
+	}
+
+	const features = useRef(null);
+	const firstFeature = useRef(null);
+	const secondFeature = useRef(null);
+	const thirdFeature = useRef(null);
+
+	let matchMedia = gsap.matchMedia();
+	let breakPoint = 769;
+
+	matchMedia.add(`(max-width: ${breakPoint - 1}px)`, () => {
+			useEffect(() => {
+				gsap.registerPlugin(ScrollTrigger);
+				gsap.to(".firstFeature", {
+					x: 600,
+					duration: 0.5,
+					scrollTrigger: {
+						trigger: ".firstFeature",
+						start: "center 90%",
+					},
+				});
+				gsap.to(".secondFeature", {
+					x: 600,
+					duration: 0.5,
+					scrollTrigger: {
+						trigger: ".secondFeature",
+						start: "center 90%",
+					},
+				});
+				gsap.to(".thirdFeature", {
+					x: 600,
+					duration: 0.5,
+					scrollTrigger: {
+						trigger: ".thirdFeature",
+						start: "center 90%",
+					},
+				});
+			}, []);
+
+			return () => {
+				matchMedia.revert();
+			};
+		}
+	);
+
+	matchMedia.add(`(min-width: ${breakPoint}px)`, () => {
+		useEffect(() => {
+			gsap.registerPlugin(ScrollTrigger);
+			gsap.to(".features", {
+				duration: 0.5,
+				ease: "fadeIn",
+				scrollTrigger: {
+					trigger: ".features",
+					start: "center 90%",
+				},
+			});
+		}, []);
+
+		return () => {
+			matchMedia.revert();
+		};
+	}
+);
 
 	return (
 		<div id="featuresContainer">
 			<div className="text titleText">Features</div>
-			<div className="features">
-				<FeatureCard
-					Component={<FaRegLightbulb size={50} color={iconTheme}/>}
-					Feature={"Creativity"}
-					Description={"You can do whatever you want"}
-                    theme = {theme}
-				/>
-				<FeatureCard
-					Component={<FaEarthAmericas size={50}  color={iconTheme}/>}
-					Feature={"Worldwide"}
-					Description={"Mr. 305 Mr. Worldwide"}
-                    theme = {theme}
-				/>
-				<FeatureCard
-					Component={<SlWallet size={50}  color={iconTheme}/>}
-					Feature={"Free"}
-					Description={"Almost free"}
-                    theme = {theme}
-				/>
+			<div className="features" ref={features}>
+				<div className="firstFeature" ref={firstFeature}>
+					<FeatureCard
+						Component={
+							<FaRegLightbulb size={50} color={iconTheme} />
+						}
+						Feature={"Creativity"}
+						Description={"You can do whatever you want"}
+						theme={theme}
+					/>
+				</div>
+				<div className="secondFeature" ref={secondFeature}>
+					<FeatureCard
+						Component={
+							<FaEarthAmericas size={50} color={iconTheme} />
+						}
+						Feature={"Worldwide"}
+						Description={"Mr. 305 Mr. Worldwide"}
+						theme={theme}
+					/>
+				</div>
+				<div className="thirdFeature" ref={thirdFeature}>
+					<FeatureCard
+						Component={<SlWallet size={50} color={iconTheme} />}
+						Feature={"Free"}
+						Description={"Almost free"}
+						theme={theme}
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- client/src/components/landingPage/hero.jsx
+++ client/src/components/landingPage/hero.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useRef } from 'react';
 import hero from '../../assets/hero.svg'
 import "../../css/landingPage/hero.css"
+import { gsap, ScrollTrigger } from "gsap/all";
 
 function HeroContainer() {
 
+    const textSideRef = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to(".textSide", {
+            x: 800,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".textSide",
+                start: "center 90%",
+            }
+        });
+    }, []);
+
     return (
         <div id="heroContainer">
-            <div className="textSide">
+            <div ref={textSideRef} className="textSide">
                 <div className="text heroBig">Unlock a World of Skills at SkillBuddy</div>
                 <div className="text heroSmall">Where Learning Meets Community</div>
                 <button className="secondary text heroButton">Sign Up</button>
