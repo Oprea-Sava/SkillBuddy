a87de17dfec335254905cc4f12878341fb1e8904
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-12-12 21:50:30 +0200

    Fixed animation bug by downgrading gsap version

--- client/src/components/landingPage/features.jsx
+++ client/src/components/landingPage/features.jsx
@@ -4,7 +4,7 @@ import { FaRegLightbulb } from "react-icons/fa6";
 import { FaEarthAmericas } from "react-icons/fa6";
 import { SlWallet } from "react-icons/sl";
 import { useTheme } from "../../themeContext";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { gsap, ScrollTrigger } from "gsap/all";
 
 function Features() {
@@ -17,79 +17,47 @@ function Features() {
 		iconTheme = "#bbcbff";
 	}
 
-	const features = useRef(null);
+
 	const firstFeature = useRef(null);
 	const secondFeature = useRef(null);
 	const thirdFeature = useRef(null);
-
 	let matchMedia = gsap.matchMedia();
 	let breakpoint = 769;
 	gsap.registerPlugin(ScrollTrigger);
 
-	useEffect(() => {
-		matchMedia.add(
-			{
-				isMobile: `(max-width: ${breakpoint}px)`,
-				isDesktop: `(min-width:  ${breakpoint + 1}px)`,
-			},
-			(context) => {
-				let { isDesktop, isMobile } = context.conditions;
-				if (isMobile) {
-					gsap.to(firstFeature.current, {
-						x: 600,
-						duration: 0.5,
-						scrollTrigger: {
-							trigger: firstFeature.current,
-							start: "top 80%",
-							end: "bottom 20%",
-						},
-						
-					});
-					gsap.to(secondFeature.current, {
-						x: 600,
-						duration: 0.5,
-						scrollTrigger: {
-							trigger: secondFeature.current,
-							start: "top 80%",
-							end: "bottom 20%",
-						},
-					});
-					gsap.to(thirdFeature.current, {
-						x: 600,
-						duration: 0.5,
-						scrollTrigger: {
-							trigger: thirdFeature.current,
-							start: "top 80%",
-							end: "bottom 20%",
-						},
-					});
-				}
-				if (isDesktop) {
-					gsap.to(features.current, {
-						opacity: 0,
-						y: 50,
-						duration: 0.5,
-						scrollTrigger: {
-							trigger: features.current,
-							start: "top 80%",
-							end: "bottom 20%",
-							toggleActions: "reverse none none play",
-						},
-						stagger: 0.2,
-					});
-				}
+	useLayoutEffect(() => {
+	const features = [firstFeature.current, secondFeature.current, thirdFeature.current];
+	matchMedia.add(
+		{
+			isMobile: `(max-width: ${breakpoint}px)`,
+			isDesktop: `(min-width:  ${breakpoint + 1}px)`,
+		},
+		(context) => {
+			let { isMobile } = context.conditions;
+			features.forEach(feature => {
+				gsap.to(feature, {
+					opacity: isMobile ? 1 : 0,
+					x: isMobile ? 600 : 0,
+					y: isMobile ? 0 : 100,
+					duration: 0.5,
+					scrollTrigger: {
+						trigger:feature,
+						start:"top 80%",
+						end: "bottom 20%",
+						toggleActions: isMobile ? "play reverse play reverse" : "reverse play  play"
+					}
+				})
+			});
+		}
+	);
+	return () => matchMedia.revert()
 
-				return () => {
-					
-				};
-			}
-		);
 	}, []);
 
 	return (
 		<div id="featuresContainer">
 			<div className="text titleText">Features</div>
-			<div className="features" ref={features}>
+			<div className="features" >
 				<div className="firstFeature" ref={firstFeature}>
 					<FeatureCard
 						Component={
