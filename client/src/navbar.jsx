463b47d739c96506ff726acf191a972ec65a4367
Sava2901 <savasergiu2006@gmail.com>
2024-02-06 14:42:15 +0200

    fixed bugs in navbar

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -11,34 +11,59 @@ import { useNavigate } from "react-router-dom";
 import placeholder from "./assets/paceholder2.png";
 import { useTheme } from "./themeContext";
 
-function Navbar({dataChange}) {
-    const { toggleTheme } = useTheme();
-    const [isOpen, setIsOpen] = useState(false)
-    const [userImg, setUserImg] = useState()
-    const navigate = useNavigate();
-    useEffect(() => {
+function Navbar({ dataChange }) {
+	const { toggleTheme } = useTheme();
+	const [isOpen, setIsOpen] = useState(false);
+	const [userImg, setUserImg] = useState();
+	const navigate = useNavigate();
+	const { theme } = useTheme();
+	const [y, setY] = useState(0);
+
+	let iconTheme = "dark";
+	if (theme == "light") {
+		iconTheme = "black";
+	} else {
+		iconTheme = "#bbcbff";
+	}
+
+	const handleNavigation = (e) => {
+		const window = e.currentTarget;
+		if (y != window.scrollY) {
+			setIsOpen(false);
+		}
+		setY(window.scrollY);
+	};
+
+	useEffect(() => {
+		setY(window.scrollY);
+
+		window.addEventListener("scroll", (e) => handleNavigation(e));
+	}, []);
+
+	useEffect(() => {
 		const fetchUserData = async () => {
-            try {
-              const token = localStorage.getItem('token');
-              const response = await fetch(`http://localhost:5000/api/users/retrieve/${token}`);
-              if (!response.ok) {
-                  throw new Error(`HTTP error! Status: ${response.status}`);
-                }
+			try {
+				const token = localStorage.getItem("token");
+				const response = await fetch(
+					`http://localhost:5000/api/users/retrieve/${token}`
+				);
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
 				const blob = await response.blob();
 				const imageUrl = URL.createObjectURL(blob);
 				setUserImg(imageUrl);
-            } catch (error) {
-              console.error('Error fetching user data:', error);
-            }
-          };
-        if(isAuthenticated()){
-		    fetchUserData();
-        }
-        
-    }, [dataChange]);
-    function goToDashboard(){
-        navigate("/dashboard")
-    }
+			} catch (error) {
+				console.error("Error fetching user data:", error);
+			}
+		};
+		if (isAuthenticated()) {
+			fetchUserData();
+		}
+	}, [dataChange]);
+	function goToDashboard() {
+		navigate("/dashboard/myprofile");
+	}
 
 	function goToSignUp() {
 		navigate("/signup");
@@ -58,7 +83,7 @@ function Navbar({dataChange}) {
 				<img className="logoText" src={logoText} alt="SkillBuddy" />
 			</div>
 			<nav className="navContainer">
-				<a className="navbarButton text" href="/">
+				<a className="navbarButton text" href="/dashboard">
 					Home
 				</a>
 				<a className="navbarButton text" href="/courses">
@@ -101,23 +126,54 @@ function Navbar({dataChange}) {
 			</nav>
 			<div className="menu" onClick={handleNav}>
 				{isOpen ? (
-					<AiOutlineClose size={20} />
+					<AiOutlineClose size={20} color={iconTheme} />
 				) : (
-					<AiOutlineMenu size={20} />
+					<AiOutlineMenu size={20} color={iconTheme} />
 				)}
 			</div>
 			<nav className="navContainerMenu" style={menuStyle}>
 				<div className="logoMenu">
 					<img src={logo} alt="logo" />
 				</div>
-				<button className="navbarButton home text">Home</button>
-				<button className="navbarButton courses text">Courses</button>
-				<button className="navbarButton skillExchange text">
+				<a className="navbarButton text" href="/dashboard">
+					Home
+				</a>
+				<a className="navbarButton text" href="/courses">
+					Courses
+				</a>
+				<a className="navbarButton text" href="/">
 					Skill Exchange
-				</button>
-				<button className="navbarButton aboutUs text">About Us</button>
-				<button className="navbarButton contact text">Contact</button>
-				<button className="primary signUp text">Sign up</button>
+				</a>
+				<a className="navbarButton text" href="/">
+					About Us
+				</a>
+				<a className="navbarButton text" href="/">
+					Contact
+				</a>
+				{isAuthenticated() ? (
+					userImg ? (
+						<img
+							className="profileImg"
+							src={userImg}
+							alt=""
+							onClick={goToDashboard}
+						/>
+					) : (
+						<img
+							className="profileImg"
+							src={placeholder}
+							alt=""
+							onClick={goToDashboard}
+						/>
+					)
+				) : (
+					<button
+						className="secondary signUp text"
+						onClick={goToSignUp}
+					>
+						Sign up
+					</button>
+				)}
 			</nav>
 		</div>
 	);
